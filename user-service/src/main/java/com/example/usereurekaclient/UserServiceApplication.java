ac669fb5a04fbed73f4ab80c416959af0dd04c0e
caohanren <34908820+caohanren@users.noreply.github.com>
2020-08-17 15:50:57 +0800

    Update UserServiceApplication.java

--- user-service/src/main/java/com/example/usereurekaclient/UserServiceApplication.java
+++ user-service/src/main/java/com/example/usereurekaclient/UserServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class UserServiceApplication {
-    //测试
+    //测试22
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
